e024d16c97a179723ed86cb8ac1b9bf18e963444
adimadd9 <denis.doncila2003@gmail.com>
2026-09-13 15:19:29 +0300

    Clean MainForm.cs

--- SRV Lab1/MainForm.Designer.cs
+++ SRV Lab1/MainForm.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             lblEquation = new Label();
             txtEquation = new TextBox();
             lblX0 = new Label();
@@ -205,17 +205,17 @@
             // 
             // chartResults
             // 
-            chartArea1.Name = "MainArea";
-            chartResults.ChartAreas.Add(chartArea1);
+            chartArea2.Name = "MainArea";
+            chartResults.ChartAreas.Add(chartArea2);
             chartResults.Dock = DockStyle.Fill;
-            legend1.Name = "Legend";
-            chartResults.Legends.Add(legend1);
+            legend2.Name = "Legend";
+            chartResults.Legends.Add(legend2);
             chartResults.Location = new Point(0, 0);
             chartResults.Name = "chartResults";
-            series1.ChartArea = "MainArea";
-            series1.Legend = "Legend";
-            series1.Name = "Series1";
-            chartResults.Series.Add(series1);
+            series2.ChartArea = "MainArea";
+            series2.Legend = "Legend";
+            series2.Name = "Series1";
+            chartResults.Series.Add(series2);
             chartResults.Size = new Size(525, 443);
             chartResults.TabIndex = 16;
             chartResults.Text = "chart1";
--- SRV Lab1/MainForm.cs
+++ SRV Lab1/MainForm.cs
@@ -12,41 +12,6 @@ namespace SRV_Lab1
             InitializeComponent();
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblStep_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_2(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lblEquation_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             // 1. Citim si validam input-ul
