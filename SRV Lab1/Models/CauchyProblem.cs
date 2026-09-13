2831a93151b75bf4f020633625659bed0e0e055c
adimadd9 <denis.doncila2003@gmail.com>
2026-09-13 14:16:33 +0300

    Add models for data processing

---
+++ SRV Lab1/Models/CauchyProblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRV_Lab1.Models
+{
+    /// <summary>
+    /// Contine toate datele de intrare ale problemei Cauchy:
+    /// du/dx = f(x, u), cu u(x0) = u0, pe intervalul [x0, xEnd], cu pasul h.
+    /// </summary>
+    internal class CauchyProblem
+    {
+        public string EquationText { get; set; }
+        public double X0 { get; set; }
+        public double U0 { get; set; }
+        public double XEnd { get; set; }
+        public double Step { get; set; }
+
+        // optional - poate fi null sau string gol daca userul nu a completat-o
+        public string? AnalyticSolutionText { get; set; }
+
+        public bool HasAnalyticSolution => !string.IsNullOrWhiteSpace(AnalyticSolutionText);
+    }
+}
+
---
+++ SRV Lab1/Models/ResultPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRV_Lab1.Models
+{
+    /// <summary>
+    /// Reprezinta un singur punct calculat: valoarea lui x, 
+    /// valoarea aproximativa (numerica), si optional valoarea exacta (analitica).
+    /// </summary>
+    public class ResultPoint
+    {
+        public double X { get; set; }
+        public double ApproxValue { get; set; }
+        public double? ExactValue { get; set; }
+
+        public double? Error { get; set; }
+        public ResultPoint(double x, double approxValue)
+        {
+            X = x;
+            ApproxValue = approxValue;
+        }
+
+    }
+}
